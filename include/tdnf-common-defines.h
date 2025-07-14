5584eb2bad0ac7cb5b4ceff6d29d515056aa4661
nicesoft-labs <info@ncsgp.ru>
2025-07-14 16:10:48 +0300

    Update tdnf-common-defines.h

--- include/tdnf-common-defines.h
+++ include/tdnf-common-defines.h
@@ -80,6 +80,15 @@
         }                                       \
     } while(0)
 
+/* Color escape sequences */
+#define COLOR_RED     "\033[1;31m"
+#define COLOR_GREEN   "\033[1;32m"
+#define COLOR_YELLOW  "\033[1;33m"
+#define COLOR_BLUE    "\033[1;34m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN    "\033[1;36m"
+#define COLOR_RESET   "\033[0m"
+
 #define LOG_INFO    0
 #define LOG_ERR     1
 #define LOG_CRIT    2
