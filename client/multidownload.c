8b38e29995a45574c3faadf4ae742c7aeb2ff334
nicesoft-labs <info@ncsgp.ru>
2025-07-15 18:26:03 +0300

    Update multidownload.c

--- client/multidownload.c
+++ client/multidownload.c
@@ -117,7 +117,8 @@ progress_cb(
         int up = g_md_prog.nRows - 1 - pData->row;
         if(up > 0)
             printf("\033[%dA", up);
-        
+            printf("\r");
+       
         if (GlobalGetColor())
         {
             pr_info("%-20s " TDNF_COLOR_GREEN "[%s]" TDNF_COLOR_RESET " %3u%% %ld %ld",
@@ -129,7 +130,7 @@ progress_cb(
         }
         else
         {
-            pr_info("%-20s [%s] %3u%% %ld %ld\r",
+            pr_info("%-20s [%s] %3u%% %ld %ld",
                     pData->pszData,
                     bar,
                     dPercent,
