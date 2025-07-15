127ab8149ef6963330abf65b72e7a86744562c29
nicesoft-labs <info@ncsgp.ru>
2025-07-15 18:27:14 +0300

    Update multidownload.c

--- client/multidownload.c
+++ client/multidownload.c
@@ -115,9 +115,10 @@ progress_cb(
         memset(bar + filled, ' ', barw - filled);
         bar[barw] = '\0';
         int up = g_md_prog.nRows - 1 - pData->row;
-        if(up > 0)
+        if(up > 0) {
             printf("\033[%dA", up);
             printf("\r");
+        }
        
         if (GlobalGetColor())
         {
