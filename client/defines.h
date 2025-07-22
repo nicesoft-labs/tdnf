e0a4cbcdf796df66bc3470e71766e76aa5b959b8
nicesoft-labs <me@sbelikov.ru>
2025-07-22 09:08:41 +0300

    Update defines.h

--- client/defines.h
+++ client/defines.h
@@ -91,6 +91,8 @@ typedef enum
 #define TDNF_CONF_KEY_OPENMAX             "openmax"
 #define TDNF_CONF_KEY_CHECK_UPDATE_COMPAT "dnf_check_update_compat"
 #define TDNF_CONF_KEY_DISTROSYNC_REINSTALL_CHANGED "distrosync_reinstall_changed"
+#define TDNF_CONF_KEY_PARALLEL_DOWNLOADS  "parallelDownloads"
+
 
 //Repo file key names
 #define TDNF_REPO_KEY_BASEURL             "baseurl"
@@ -118,6 +120,8 @@ typedef enum
 
 //setopt keys
 #define TDNF_SETOPT_KEY_REPOSDIR          "reposdir"
+#define TDNF_SETOPT_KEY_PARALLEL_DOWNLOADS "parallelDownloads"
+
 
 //file names
 #define TDNF_REPO_METADATA_MARKER         "lastrefresh"
@@ -145,6 +149,8 @@ typedef enum
 #define TDNF_REPO_METADATA_EXPIRE_NEVER   "never"
 
 #define TDNF_DEFAULT_OPENMAX              1024
+#define TDNF_DEFAULT_PARALLEL_DOWNLOADS   4
+
 
 // repo default settings
 #define TDNF_REPO_DEFAULT_ENABLED            0
