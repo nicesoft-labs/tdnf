5799d1dca8a269b8639e8e35decb801b2972bce8
nicesoft-labs <me@sbelikov.ru>
2025-08-04 23:54:32 +0300

    Update tdnf-common-defines.h

--- include/tdnf-common-defines.h
+++ include/tdnf-common-defines.h
@@ -85,9 +85,10 @@
 #define COLOR_GREEN   "\x1b[32m"
 #define COLOR_YELLOW  "\x1b[33m"
 #define COLOR_BLUE    "\x1b[34m"
+#define COLOR_PURPLE  "\x1b[35m"
 #define COLOR_RESET   "\x1b[0m"
 #define COLOR_BOLD_RED "\x1b[1;31m"
-
+#define COLOR_BOLD_PURPLE "\x1b[1;35m"
 
 
 #define LOG_INFO    0
